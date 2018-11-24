502ce1613611a67f5322787cf8150c4f19d4d611
user200 <user200@DESKTOP-1QT0TEM>
2018-11-24 18:14:16 +0300

    Add project files.

---
+++ SharpForSchoolForm/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SharpForSchoolForm
+{
+    public partial class MyButtonClass : Form
+    {
+        private Button mrButton;
+        public MyButtonClass()
+        {
+            mrButton = new Button();
+            mrButton.Text = "Click me";
+            mrButton.Top = 100;
+            mrButton.Left = 100;
+            mrButton.Height = 50;
+            mrButton.Width = 70;
+            mrButton.Click += new System.EventHandler(MyButtonClickEventHandler);
+            this.MouseDown += new MouseEventHandler(TheMouseIsDown);
+            //this.MouseClick += new MouseEventHandler(TheMouseWasClicked);
+            this.MouseMove += new MouseEventHandler(TheMouseMoved);
+
+            this.Controls.Add(mrButton);
+        }
+
+        void MyButtonClickEventHandler(object sender, EventArgs e)
+        {
+            mrButton.Text = "You click me!";
+        }
+
+        void TheMouseIsDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Text = "Click left mouse button";
+            }
+        }
+
+        void TheMouseWasClicked(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                this.Width = this.Width + 100;
+            else if (e.Button == MouseButtons.Right)
+                this.Width = this.Width - 100;
+        }
+
+        void TheMouseMoved(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen redPen = new Pen(Color.Red, 3);
+
+            g.DrawEllipse(redPen, e.X, e.Y, 40, 40);
+
+            g.Dispose();
+        }
+    }
+}
---
+++ SharpForSchoolForm2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpForSchoolForm2
+{
+    public partial class Form1 : Form
+    {
+        ListBox listBox1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Size = new Size(400, 400);
+
+            PictureBox pictureBox1 = new PictureBox();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.ClientSize = new Size(this.Width, 150);
+            Bitmap image1 = new Bitmap("C:\\Users\\user200\\Downloads\\image.gif");
+            pictureBox1.Image = (Image)image1;
+            this.Controls.Add(pictureBox1);
+
+            Button button1 = new Button();
+            button1.Location = new Point(150, 160);
+            button1.Size = new Size(100, 30);
+            button1.Text = "Click me!";
+            button1.Click += new EventHandler(button1_Click);
+
+            this.Controls.Add(button1);
+
+            listBox1 = new ListBox();
+            listBox1.Location = new Point(20, 200);
+            listBox1.Size = new Size(100, 100);
+            listBox1.Items.Add("Forest");
+            listBox1.Items.Add("Steppe");
+            listBox1.Items.Add("Lake");
+            listBox1.Items.Add("Sea");
+            listBox1.Items.Add("Ocean");
+            listBox1.SelectedIndex = 2;
+            this.Controls.Add(listBox1);
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "You choice " + listBox1.SelectedItem, "Warning", MessageBoxButtons.OK);
+        }
+
+        void InInitializeComponent()
+        {
+            this.SuspendLayout();
+
+            this.BackColor = SystemColors.Control;
+            this.ClientSize = new Size(292, 273);
+            this.Name = "MyForm";
+
+            this.ResumeLayout(false);
+        }
+    }
+}
